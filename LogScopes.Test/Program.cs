6087d43b207e764e3d1f46e9dd74686dfc0e79ba
mi.ashrafi <mi.ashrafi@alibaba.ir>
2022-05-27 00:42:51 +0430

    Add project files.

---
+++ LogScopes.Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static Task Main(string[] args) =>
+    CreateHostBuilder(args).Build().RunAsync();
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(options =>
+            {
+                options.AddJsonFile("appsettings.json");
+            })
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<Worker>();
+            });
+
+    }
+}
---
+++ LogScopes.Test/Worker.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using (_logger.BeginScope("OrderId: {OrderId}", 1234))
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    await Task.Delay(1000, stoppingToken);
+                }
+            }
+        }
+    }
+}
